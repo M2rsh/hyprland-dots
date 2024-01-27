70fc52def2d6012bc5893147a03e2852d6d150ea
Che <mariuszbubak10@gmail.com>
2024-01-27 19:02:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ install_function () {
     rofi-lbonn-wayland-git rofi-emoji-abi8 wtype \
     hyprpicker swww waypaper-git
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    mv .config/* ~/.config/
+    mv .local/* ~/.local/
 }
 
 echo "It is not recommended to run with pre existing configs"
