c17d032720ff172f13c818ec0486ed5ad7dcc4d1
M2rsh <mariuszbubak10@gmail.com>
2024-01-27 18:49:46 +0100

    Rofi clipboard history

---
+++ .config/rofi/clipboard.sh
@@ -0,0 +1,5 @@
+rofi -modi clipboard:$HOME/.config/rofi/cliphist-rofi-img \
+                -show clipboard \
+                -theme $HOME/.config/rofi/clipboard.rasi \
+                -hover-select \
+                -i
--- .config/rofi/emoji.sh
+++ .config/rofi/emoji.sh
@@ -1,8 +1,9 @@
-wtype $(rofi \
-    -emoji-format "{emoji}" \
-    -emoji-mode stdout \
-    -show emoji \
-    -hover-select \
-    -i \
-    -theme $HOME/.config/rofi/emoji.rasi)
+rofi_output=$(rofi -emoji-format "{emoji}" \
+                   -emoji-mode stdout \
+                   -show emoji \
+                   -hover-select \
+                   -i \
+                   -theme $HOME/.config/rofi/emoji.rasi)
 
+wtype "$rofi_output"
+wl-copy "$rofi_output"
